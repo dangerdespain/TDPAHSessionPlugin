76e9abe95a0720ae6657702988c08b5f68a0463f
neilstuartcraig <neil.craig@endava.com>
2014-11-24 15:08:58 +0000

    v0.1.3-alpha

---
+++ scripts/NPMPostInstallScript.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script is solely for NPM installs, to be run after the initial "npm install"
+# it is not used for travis tests, see travisScript.sh for that
+
+
+# TODO: Work out a way to automatically add the plugin name to the <root>/config/api.js in general.paths []
+
+
+# We'll wrap this in a condition so it can't fail - e.g. on travis
+if [ -e ./config/TDPAHSessionPluginConfigDefaults.js ]
+	then 
+		# Create (AH) config/plugins dir if it doesn't exist
+		mkdir -p "../../config/plugins";
+
+		# copy config file to AH
+		cp ./config/TDPAHSessionPluginConfigDefaults.js ../../config/plugins/AHTDPSessionPlugin.js;
+fi
---
+++ scripts/travisScript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+echo "hoi";
+
+# Install NPM modules
+npm install;
+
+# Generate actionhero project skeleton
+./node_modules/.bin/actionhero generate;
+
+# Create (AH) config/plugins dir
+mkdir -p "./config/plugins";
+
+# Move the existing config file to the plugins dir created above
+mv ./config/TDPAHSessionPluginConfigDefaults.js ./config/plugins/AHTDPSessionPlugin.js;
